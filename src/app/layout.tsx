d4ee04a79b6936e04082364f5c0cd9be3f355dd8
Tim Cieplowski <tim.cieplowski@gmail.com>
2024-12-18 01:27:04 -0700

    Add google analytcs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -124,6 +125,8 @@ export default function RootLayout({
             </div>
           </div>
         </footer>
+
+        <GoogleAnalytics gaId="G-6ENCB6TNZG" />
       </body>
     </html>
   );
