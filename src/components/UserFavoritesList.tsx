abad0d3cd514f3f417fa497c7688926defd4f0cf
Tim Cieplowski <tim.cieplowski@gmail.com>
2024-12-18 16:21:56 -0700

    add checkboxes

--- src/components/UserFavoritesList.tsx
+++ src/components/UserFavoritesList.tsx
@@ -13,18 +13,25 @@ export function UserFavoritesList({
   return (
     <div className="flex gap-2 justify-center flex-wrap">
       {movies.map((movie) => (
-        <button
+        <div
           key={movie.slug}
           className={
-            "p-2 rounded border-solid border-2 " +
+            "p-2 rounded border-solid border-2 cursor-pointer " +
             (selectedMovieSlugs.includes(movie.slug)
               ? "border-orange-600 hover:border-orange-400"
               : "border-shark-600 hover:border-shark-400 text-shark-200")
           }
           onClick={() => onMovieSelected(movie.slug)}
         >
+          <input
+            type="checkbox"
+            className="cursor-pointer mr-2"
+            checked={selectedMovieSlugs.includes(movie.slug)}
+            readOnly
+          />
+
           {movie.title}
-        </button>
+        </div>
       ))}
     </div>
   );
